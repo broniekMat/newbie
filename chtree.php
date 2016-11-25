a765c0502e65c43f5502e9577d5e4c3c33e0158e
Łukasz Węgrzynkiewicz <wegrzynkiewicz.lukasz@gmail.com>
2016-11-25 20:03:25 +0100

    Fixed

--- chtree.php
+++ chtree.php
@@ -1,34 +1,20 @@
 <?php
 
 for ($i=1; $i<=100; $i++) {
+    $fiz = $i % 3;
+    $buzz = $i % 5;
 
-$fiz = $i % 3;
-$buzz = $i % 5;  
-
-$str = "";
+    $str = "";
 
     if ($fiz == 0 && $buzz == 0) {
-        
         $str.= "FizzBuzz";
-
-    } else if ($buzz == 0) {
-        
+    } elseif ($buzz == 0) {
         $str.= "buzz";
-
-    } else if ($fiz == 0 ) {
-
-       $str.= "fizz";
-
+    } elseif ($fiz == 0) {
+        $str.= "fizz";
     } else {
-
         echo $str.= $i;
     }
 
-
     echo $str."</br>";
-
 }
-
-
-
-
